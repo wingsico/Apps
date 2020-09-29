5eb2a3cebfc64cbcec81dcaa2f309bac323943d9
zhongweijie.x <zhongweijie.x@bytedance.com>
2020-09-29 10:44:10 +0800

    feat: 自定义字体

---
+++ lib/common/my_icons.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyIcons {
+  static const IconData Confirm = const IconData(
+    0xe6e3,
+    fontFamily: 'myIcon',
+    matchTextDirection: true,
+  );
+
+  static const IconData Status = const IconData(
+    0xe6e4,
+    fontFamily: 'myIcon',
+    matchTextDirection: true,
+  );
+}
